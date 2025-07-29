19a6a4072d4a13b15a9b40d2b866f6c36bc97d03
Arun Vashisth <arunvashisth80@GMAIL.COM>
2025-07-29 14:36:00 +0530

    Add files via upload

---
+++ assignments/13/id-card/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import IDCard from './components/IDCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="main-title">ID Card</h1>
+      <IDCard />
+    </div>
+  );
+}
+
+export default App;
+
+
